7ead896b716049193f3fb8d596a7908915fd0c7a
davidsexto <dsextogarcia2@gmail.com>
2026-01-13 09:43:00 +0100

    hjdf

--- PHP/Más_PHP/ejercicio_gemini.php
+++ PHP/Más_PHP/ejercicio_gemini.php
@@ -1,5 +1,13 @@
-//ejercicio que comprueba la edad: 
+//ejercicio que comprueba la nota: 
 
 <?php 
+    $nota = 10;
+    if($nota == 10){
+        echo "ENHORABUENA ESTÁS SOBRESALIENTE";
+    }elseif ($nota >= 5) {
+        echo "Estás aprobado";
+    }else {
+        echo "Has suspendido :(";
+    }
     
 ?>
